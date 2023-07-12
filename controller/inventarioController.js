5893d8c7df458f1fda82096726157d0aedc617b1
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-07-12 10:36:58 -0500

    feat: :sparkles: endpoint registro inventario
    Se agrega endpoint para ingresar registros en el inventario

--- controller/inventarioController.js
+++ controller/inventarioController.js
@@ -9,7 +9,7 @@ const inventariosget = (req,res) => {
         })
 };
 
-const inventariospost = (req, res) => {
+const productopost = (req, res) => {
   
     // Insertar el producto en la tabla "productos"
     req.conexion.query(
@@ -36,4 +36,53 @@ const inventariospost = (req, res) => {
          
 }
 
-export {inventariosget, inventariospost};
+
+const inventariospost = (req, res) => {
+    const { id_producto, id_bodega, cantidad } = req.body;
+  
+    // Verificar si la combinación de id_producto e id_bodega ya existe en la tabla de inventarios
+    req.conexion.query(
+      'SELECT * FROM inventarios WHERE id_producto = ? AND id_bodega = ?',
+      [id_producto, id_bodega],
+      (error, results) => {
+        if (error) {
+          console.error(error);
+          res.status(500)
+        }
+  
+        if (results.length === 0) {
+          // La combinación es nueva, realizar un INSERT en la tabla de inventarios
+          req.conexion.query(
+            'INSERT INTO inventarios (id_producto, id_bodega, cantidad) VALUES (?, ?, ?)',
+            [id_producto, id_bodega, cantidad],
+            (error, results) => {
+              if (error) {
+                console.error(error);
+                res.status(500).send(error);
+              }
+              res.status(200).send(results);
+            }
+          );
+        } else {
+          // La combinación ya existe, realizar un UPDATE en la tabla de inventarios sumando la cantidad existente con la cantidad nueva
+          const cantidadExistente = results[0].cantidad;
+          const nuevaCantidad = cantidadExistente + cantidad;
+          console.log(results);
+          req.conexion.query(
+            'UPDATE inventarios SET cantidad = ? WHERE id_producto = ? AND id_bodega = ?',
+            [nuevaCantidad, id_producto, id_bodega],
+            (error, results) => {
+              if (error) {
+                console.error('Error al actualizar la tabla de inventarios:', error);
+                res.status(500).send(error);
+              }
+              res.status(200).send(results);
+            }
+          );
+        }
+      }
+    );
+  };
+  
+
+export {inventariosget, inventariospost, productopost};
--- router/inventarioRouter.js
+++ router/inventarioRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import {inventariosget, inventariospost} from '../controller/inventarioController.js'
+import {inventariosget, productopost, inventariospost} from '../controller/inventarioController.js'
 import conexion_db from '../middleware/conexion_db.js';
 
 const inventarios = Router();
 
 inventarios.get('/', conexion_db, inventariosget);
+inventarios.post('/producto', conexion_db, productopost);
 inventarios.post('/', conexion_db, inventariospost);
 
 
