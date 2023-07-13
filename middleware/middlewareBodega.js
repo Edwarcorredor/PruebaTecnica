6947e46691969d7376def90f44309f4dabadf657
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-07-13 17:21:58 -0500

    feat: :art: direccion middleware

--- middleware/middlewareBodega.js
+++ middleware/middlewareBodega.js
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 import { plainToClass } from 'class-transformer';
-import {bodegas} from "./controller/bodegas.js"
+import {bodegas} from "../controller/bodegas.js"
 
 const middlewareBodega = (req, res, next) => {
     try{
         let data = plainToClass(bodegas, req.body);
         req.body = JSON.parse(JSON.stringify(data));
+        
         next();
     } catch(Error){
         res.send("Error");
