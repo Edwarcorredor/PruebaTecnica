ba8d502b03d41faf2028f345cb6422bf28829550
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-07-12 22:58:16 -0500

    feat: :art: Orden y nombres
    Se cambian el orden y nombres, se añaden tambien comentarios para entender mejor el codigo

--- controller/bodegasController.js
+++ controller/bodegasController.js
@@ -1,14 +1,23 @@
-const bodegasget = (req, res) => {
+/**
+ * @param {atributo en el cual se tiene la conexion con la base de datos} req.conexion 
+ * @param {respuesta que le damos al usuario} res 
+*/
+const getBodegas = (req, res) => {
+    /**
+     **getBodegas es una funcion para obtener la informacion en la tabla de bodegas en orden alfabetico 
+    */
     req.conexion.query(
         /*sql*/`SELECT * FROM bodegas ORDER BY nombre ASC`,
         (error, data,fils) => {
             res.send(data);
-        })
+    })
 };
 
-
-const bodegaspost = (req, res) => {
-
+const postBodegas = (req, res) => {
+    /**
+    **postBodegas es una funcion que permite crear bodegas recibiendo los siguientes variables de entrada:
+    **nombre, id_responsable, estado, created_by, update_by, updated_at, deleted_at
+    */
     req.conexion.query(
         /*sql*/`INSERT INTO bodegas SET ?`,
         req.body,
@@ -24,4 +33,4 @@ const bodegaspost = (req, res) => {
     );      
 }
 
-export  {bodegasget, bodegaspost};
+export  {getBodegas, postBodegas};
--- router/bodegasRouter.js
+++ router/bodegasRouter.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import conexion_db from '../middleware/conexion_db.js';
-import {bodegasget,bodegaspost} from '../controller/bodegasController.js';
+import {getBodegas,postBodegas} from '../controller/bodegasController.js';
 
 const bodegas = Router();
 
-bodegas.get('/', conexion_db, bodegasget);
-bodegas.post('/', conexion_db, bodegaspost);
+bodegas.get('/', conexion_db, getBodegas);
+bodegas.post('/', conexion_db, postBodegas);
 
 export default bodegas;
